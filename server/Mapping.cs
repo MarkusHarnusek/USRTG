dde841d16f1a8bf6e398986d4615bd40e723ad2e
Markus Harnúšek <m.harnusek@students.htl-leonding.ac.at>
2025-10-24 10:25:21 +0100

    Added MapFlagType with mapping for assetto corsa

--- server/Mapping.cs
+++ server/Mapping.cs
@@ -30,6 +30,36 @@ namespace USRTG
             }
         }
 
+        public int MapFlagType(int flagType, Enums.Game game)
+        {
+            switch (game)
+            {
+                case Enums.Game.AssettoCorsa:
+                    var flagMapping = new Dictionary<int, int>
+                    {
+                        { 0, 0 }, // No Flag
+                        { 1, 7 }, // Blue Flag
+                        { 2, 2 }, // Yellow Flag
+                        { 3, 9 }, // Black Flag
+                        { 4, 5 }, // White Flag
+                        { 5, 6 }, // Checkered Flag
+                        { 6, 9 }  // Penalty Flag
+                    };
+
+                    if (flagMapping.TryGetValue(flagType, out int mappedFlag))
+                    {
+                        return mappedFlag;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unknown flag type: " + flagType);
+                        return 0; // Default to No Flag
+                    }
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
 
     }
 }
